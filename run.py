be0f2d27f4a013b6ce756491181565e17533c8f8
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-20 21:13:07 +0000

    Add docstring to the get_valid_user_input function and create function that returns the available squares

--- run.py
+++ run.py
@@ -20,7 +20,23 @@ def print_board():
     print('\n+---+---+---+')
 
 
+def available_moves():
+    '''
+    Returns all the available squares that the player can make a move on
+    '''
+    moves = []
+
+    for row in range(3):
+        for column in range(3):
+            moves.append(board[row][column])
+    return moves
+
+
 def get_valid_user_move():
+    '''
+    Let the user pick a square
+    Check if the square is in the board
+    '''
     num_range = [1, 2, 3, 4, 5, 6, 8, 9]
     is_square_valid = False
 
@@ -30,11 +46,11 @@ def get_valid_user_move():
         if picked_square not in num_range:
             print('Invalid input, Please pick a valid square!')
             is_square_valid = False
-        else: 
+        else:
             print('Valid number')
             is_square_valid = True
 
 
-print_board()
+t = available_moves()
 
-get_valid_user_move()
+print(t)
