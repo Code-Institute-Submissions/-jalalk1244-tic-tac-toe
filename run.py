c208b961148cb67b637cdae9de8a103bf68ee1c2
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-20 15:38:02 +0000

    Cread a function that prints out a 3x3 tic tac toe game board

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+board = [
+        [' ', ' ', ' '], 
+        [' ', ' ', ' '], 
+        [' ', ' ', ' ']
+        ]
+
+WINNER = None
+
+
+def print_board():
+    '''
+    Print the tic tac toe game board
+    '''
+
+    rows = 3
+    columns = 3
+
+    for row in range(rows):
+        print('\n+---+---+---+')
+        print('|', end='')
+        for column in range(columns):
+            print('', board[row][column], end=' |')
+    print('\n+---+---+---+')
+
+print_board()
