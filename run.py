e28bb61be547a0de0c86b14dfa2a66c89b3cc1db
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-21 10:16:27 +0000

    Create a function that appends the user move to the board

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 WINNER = None
+current_player = 'X'
 
 
 def print_board():
@@ -54,7 +55,23 @@ def get_valid_user_move(moves):
     return player_move
 
 
-print_board()
-t = available_moves()
-h = get_valid_user_move(t)
-print(h)
+def append_move_to_board(move):
+    '''
+    Add the player move to the board if it is a valid move
+    Remove the square from the avaible moves
+    '''
+
+    if move >= 1 and move <= 3:
+        board[0][move-1] = current_player
+    elif move >= 4 and move <= 6:
+        board[1][move-4] = current_player
+    elif move >= 7 and move <= 9:
+        board[2][move-7] = current_player
+    del available_moves()[move - 1]
+
+
+while ' ' in board[0] or ' ' in board[1] or ' ' in board[2]:
+    print_board()
+    t = available_moves()
+    h = get_valid_user_move(t)
+    append_move_to_board(h)
