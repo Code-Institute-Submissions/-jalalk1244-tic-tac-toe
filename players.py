4b150b1ea0d7e587b0016649cb0f7ce100b329db
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-23 18:01:17 +0000

    Move the get_valid_computer_move to the computer player class

--- players.py
+++ players.py
@@ -1,5 +1,17 @@
+
+
 class ComputerPlayer:
-    pass
+    def computer_move():
+        '''
+        Returns a random computer move from the available squares 
+        '''
+        try:
+            pc = random.choice(available_squares)
+            available_squares.remove(pc)
+            return pc
+        except IndexError:
+            print()
+
 
 class UserPlayer:
     pass
