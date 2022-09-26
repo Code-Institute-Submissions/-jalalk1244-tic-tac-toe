e8048c1cc1ff19ac485a52f991656782f41a6af4
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-26 21:06:00 +0000

    Fix the errors and warnings of the players.py file

--- players.py
+++ players.py
@@ -15,12 +15,12 @@ class ComputerPlayer:
         Returns a random computer move from the available squares
         '''
         try:
-            pc = random.choice(game.available_squares)
-            game.available_squares.remove(pc)
-            return pc
+            pc_choice = random.choice(game.available_squares)
+            game.available_squares.remove(pc_choice)
+            return pc_choice
         except IndexError:
             print()
-    
+
     def get_valid_medium_computer_move(self, game):
         '''
         Reduces the randomness of the computer choice
@@ -38,32 +38,54 @@ class ComputerPlayer:
                                 [4, 5, 6],
                                 [7, 8, 9]]
 
-        # If in any of the winnig combinations, 2 of them is taken choose the third one
+        # If in any of the winnig combinations,
+        # 2 of them is taken choose the third one
         # i.e  if one and 3 is x/o choose return 2
         for way in possible_ways_to_win:
-            if (flat_board[way[0] - 1] == flat_board[way[1] - 1]) and flat_board[way[0] - 1] != ' ':
+            if (
+                    (flat_board[way[0] - 1] == flat_board[way[1] - 1])
+                    and flat_board[way[0] - 1] != ' '
+               ):
                 if way[2] in game.available_squares:
                     game.available_squares.remove(way[2])
-                    pc = way[2]
-                    return pc
-            elif (flat_board[way[0] - 1] == flat_board[way[2] - 1]) and flat_board[way[0] - 1] != ' ':
+                    pc_choice = way[2]
+                    return pc_choice
+            elif (
+                    (flat_board[way[0] - 1] == flat_board[way[2] - 1])
+                    and flat_board[way[0] - 1] != ' '
+                 ):
                 if way[1] in game.available_squares:
                     game.available_squares.remove(way[1])
-                    pc = way[1]
-                    return pc
-            elif (flat_board[way[1] - 1] == flat_board[way[2] - 1]) and flat_board[way[1] - 1] != ' ':
+                    pc_choice = way[1]
+                    return pc_choice
+            elif (
+                    (flat_board[way[1] - 1] == flat_board[way[2] - 1])
+                    and flat_board[way[1] - 1] != ' '
+                 ):
                 if way[0] in game.available_squares:
                     game.available_squares.remove(way[0])
-                    pc = way[0]
-                    return pc
-            elif flat_board[8] == ' ' or flat_board[0] == ' ' or flat_board[6] == ' ' or flat_board[2] == ' ':
-                pc = 9 if flat_board[8] == ' ' else 7 if flat_board[6] == ' ' else 3 if flat_board[2] == ' ' else 1 
-                game.available_squares.remove(pc)
-                return pc 
+                    pc_choice = way[0]
+                    return pc_choice
+            elif (
+                    flat_board[8] == ' ' or flat_board[0] == ' '
+                    or flat_board[6] == ' ' or flat_board[2] == ' '
+                 ):
+
+                if flat_board[8] == ' ':
+                    pc_choice = 9
+                elif flat_board[6] == ' ':
+                    pc_choice = 7
+                elif flat_board[2] == ' ':
+                    pc_choice = 3
+                else:
+                    pc_choice = 1
+                game.available_squares.remove(pc_choice)
+                return pc_choice
             else:
-                pc = random.choice(game.available_squares)
-                game.available_squares.remove(pc)       
-                return pc
+                pc_choice = random.choice(game.available_squares)
+                game.available_squares.remove(pc_choice)
+                return pc_choice
+
 
 class UserPlayer:
     '''
@@ -91,8 +113,6 @@ class UserPlayer:
                 else:
                     moves.insert(player_move - 1, 'X')
                     game.available_squares.remove(player_move)
-                    # print(f'\n{current_letter} has moved to square {player_move}')
-                    # print(f'\nIt is {turn[0]}:s turn')
                     is_square_valid = True
             except ValueError:
                 print(messages['not-alpha'])
